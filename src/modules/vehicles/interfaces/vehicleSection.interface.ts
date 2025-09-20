6d22a842c15cccf4bf5efa79cfa5ae2095ad894c
Hernan Yarbi <hernan.yarbi@gmail.com>
2025-09-20 18:01:34 -0300

    feat: Update logic usisig registry

--- src/modules/vehicles/interfaces/vehicleSection.interface.ts
+++ src/modules/vehicles/interfaces/vehicleSection.interface.ts
@@ -1,7 +1,8 @@
 import { FC } from "react";
 import { Vehicle } from "./vehicle";
+import { PropsWithVehicle } from "./propsWithVehicle";
 
 export interface VehicleSection {
-  component: FC;
+  component: FC<PropsWithVehicle>;
   isVisible: (vehicle: Vehicle) => boolean;
 }
--- src/modules/vehicles/views/VehicleView.tsx
+++ src/modules/vehicles/views/VehicleView.tsx
@@ -1,20 +1,19 @@
 "use client";
-import { Card } from "@/modules/common/components/atoms/Card";
 import { Container } from "@/modules/common/components/atoms/Container";
-import { FC } from "react";
-import { VehicleImage } from "../components/atoms/VehicleImage";
+import { FC, useMemo } from "react";
 import { useVehicle } from "../hooks/useVehicle";
-import { Chip } from "@/modules/common/components/atoms/Chip";
+import { getVisibleVehicleSections } from "../registries/vehicleSection.registry";
 import { VehicleHeaderSection } from "../components/molecules/sections/VehicleHeaderSection";
-import { VehicleGeneralInfoSection } from "../components/molecules/sections/VehicleGeneralInfoSection";
-import { VehicleConditionSection } from "../components/molecules/sections/VehicleConditionSection";
-import { VehicleElectricSection } from "../components/molecules/sections/VehicleElectricSection";
 
 type VehicleViewProps = {
   id: string;
 };
 export const VehicleView: FC<VehicleViewProps> = ({ id }) => {
   const { vehicle } = useVehicle(id);
+  const visibleSections = useMemo(
+    () => getVisibleVehicleSections(vehicle),
+    [vehicle]
+  );
   if (!vehicle) return <div>Loading...</div>;
   return (
     <Container className="py-8">
@@ -25,11 +24,9 @@ export const VehicleView: FC<VehicleViewProps> = ({ id }) => {
           flags={vehicle.flags}
         />
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <VehicleGeneralInfoSection vehicle={vehicle} />
-          {vehicle.flags.isElectric && vehicle?.electricDetails && (
-            <VehicleElectricSection vehicle={vehicle}/>
-          )}
-          <VehicleConditionSection vehicle={vehicle}/>
+          {visibleSections.map(({ component: ComponentSection }, index) => (
+            <ComponentSection vehicle={vehicle} key={index} />
+          ))}
         </div>
       </div>
     </Container>
